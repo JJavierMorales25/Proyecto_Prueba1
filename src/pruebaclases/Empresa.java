bfedc2eb389692ddc637b6b73b7756b1297d8587
Jose Javier Morales <111585682+JJavierMorales25@users.noreply.github.com>
2022-08-22 18:15:59 -0500

    Update Empresa.java

--- src/pruebaclases/Empresa.java
+++ src/pruebaclases/Empresa.java
@@ -3,6 +3,6 @@ package pruebaclases;
 public class Empresa
 {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo_22/08/22");
     }
 }
